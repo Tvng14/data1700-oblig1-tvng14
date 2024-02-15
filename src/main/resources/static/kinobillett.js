d2a48323e9bbc14ce9e51e2eb9d406e587979d74
Tungu2400 <142822487+Tungu2400@users.noreply.github.com>
2024-02-15 20:00:58 +0100

    Opprettet funksjoner som sjekker om det er tomme input-felter

--- src/main/resources/static/kinobillett.js
+++ src/main/resources/static/kinobillett.js
@@ -1,15 +1,45 @@
 
 function velgFilm(){
 }
+function sjekkAntallBilletter(){
+    let antallBilletter = document.getElementById("antallBilletter").value;
+    if (antallBilletter == "") {
+        document.getElementById("feilAntall").innerText = "Vennligst velg antall billetter";
+    } else {
+        document.getElementById("feilAntall").innerText = "";
+    }
+}
 function sjekkFornavn(){
+    let innFornavn = document.getElementById("innFornavn").value;
+    if (innFornavn.trim() == "") {
+        document.getElementById("feilFornavn").innerText = "Vennligst fyll inn fornavn";
+    } else {
+        document.getElementById("feilFornavn").innerText = "";
+    }
 }
 function sjekkEtternavn(){
+    let innEtternavn = document.getElementById("innEtternavn").value;
+    if (innEtternavn.trim() == "") {
+        document.getElementById("feilEtternavn").innerText = "Vennligst fyll inn etternavn";
+    } else {
+        document.getElementById("feilEtternavn").innerText = "";
+    }
 }
 function sjekkTlf(){
-
+    let innTlf = document.getElementById("innTlf").value;
+    if (innTlf.trim() == "") {
+        document.getElementById("feilTlf").innerText = "Vennligst fyll inn telefonnummer";
+    } else {
+        document.getElementById("feilTlf").innerText = "";
+    }
 }
 function sjekkEpost() {
-
+    let innEpost = document.getElementById("innEpost").value;
+    if (innEpost.trim() == "") {
+        document.getElementById("feilEpost").innerText = "Vennligst fyll ut e-post";
+    } else {
+        document.getElementById("feilEpost").innerText = "";
+    }
 }
 function bestillBillett() {
 }
