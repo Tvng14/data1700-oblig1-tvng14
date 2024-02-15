fd6818a62dd54677f00423f5e58226d0f2293b93
Tungu2400 <142822487+Tungu2400@users.noreply.github.com>
2024-02-15 16:59:04 +0100

    Lagt til style for sjekk av inputs og tilhørende javascript funksjoner for inputsene

--- src/main/resources/static/kinobillett.js
+++ src/main/resources/static/kinobillett.js
@@ -1,2 +1,18 @@
 
+function velgFilm(){
+}
+function sjekkFornavn(){
+}
+function sjekkEtternavn(){
+}
+function sjekkTlf(){
 
+}
+function sjekkEpost() {
+
+}
+function bestillBillett() {
+}
+function slettBilletter(){
+
+}
